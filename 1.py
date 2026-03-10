4926d8c4fc5371e784425cfc11ca7678f9ea34c5
MariaPelegrini <mariapelegrini.escolapj.senai@gmail.com>
2026-03-10 09:36:39 -0300

    todas operaçoes

--- 1.py
+++ 1.py
@@ -1,1 +1,21 @@
-print("hello")
+n1= int(input("Digite um numero: "))
+n2= int(input("Digite outro  numero: "))
+soma = n1 + n2
+sub = n1 - n2 
+mult = n1 * n2 
+div = n1 / n2 
+
+calculo = int(input("Agora digite 1 para soma , 2 para subtração, 3 para multiplicação e 4 para divisãp "))
+
+if calculo == 1 :
+    print(soma)
+
+if calculo == 2 :
+    print(sub)
+    
+if calculo == 3 :
+    print(mult)
+    
+if calculo == 4 :
+    print(div)
+    
